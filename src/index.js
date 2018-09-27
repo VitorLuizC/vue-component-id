1c75ec8db80aab113eb407cb5a6d1b4da5c1da6f
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-09-27 15:30:28 -0300

    :sparkles: creates plugin's source

---
+++ src/index.js
@@ -0,0 +1,40 @@
+/**
+ * A function which generates an unique ID.
+ * @typedef {function():string} IDGenerator
+ */
+
+/**
+ * Creates an ID generator function. Every execution returns a prefix text
+ * concatenated with an incremented number.
+ * @param {string} [prefix] - A text to be concatenated with an incremented number.
+ * @return {IDGenerator}
+ */
+export const createGenerator = (prefix = '') => {
+  let count = 0;
+  return () => prefix + (count++).toString(10);
+};
+
+/**
+ * Default generator function.
+ */
+const DEFAULT_GENERATOR = createGenerator();
+
+/**
+ * Install on Vue instance to generates an unique ID for every component. You
+ * can also provide your own generator.
+ * @param {Vue} Vue
+ * @param {{ generator: IDGenerator }} [options]
+ */
+const install = (Vue, options = {}) => {
+  const { generator = DEFAULT_GENERATOR } = options;
+
+  Vue.mixin({
+    data () {
+      return {
+        $id: generator()
+      };
+    }
+  });
+};
+
+export default install;
